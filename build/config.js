8a993fd0cc2e8f7280fbc74d0ff1a6294a6e5bb8
zhengoogle <zhengoogle@sina.com>
2020-03-25 19:29:49 +0800

    support plus dir

--- build/config.js
+++ build/config.js
@@ -36,7 +36,7 @@ exports.alias = {
   main: path.resolve(__dirname, '../src'),
   packages: path.resolve(__dirname, '../packages'),
   examples: path.resolve(__dirname, '../examples'),
-  'element-ui': path.resolve(__dirname, '../')
+  'element-ui': path.resolve(__dirname, './../../element-ui-plus/')
 };
 
 exports.vue = {
